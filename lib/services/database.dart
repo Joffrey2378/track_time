3e38e2c2196bbd4f64efacacda76d5e39f62fc8e
Joffrey2378 <georged.2378@gmail.com>
2020-04-30 15:26:48 +0300

    Created service class that contains paths in static strings to provide them in service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:track_time/app/home/models/job.dart';
+import 'package:track_time/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -12,7 +13,7 @@ class FirestoreDatabase implements Database {
   final String uid;
 
   Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs/job_abc/';
+    final path = APIPath.job(uid, 'job_123');
     final documentReference = Firestore.instance.document(path);
     await documentReference.setData(job.toMap());
   }
