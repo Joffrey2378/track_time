f1755ecbcca2fe6e17e5b2c9ed00b595d1596a88
Joffrey2378 <georged.2378@gmail.com>
2020-05-07 18:58:57 +0300

    Created fetching data from firebase via stream and show via stream builder

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -33,7 +33,10 @@ class JobsPage extends StatelessWidget {
   Future<void> _createJob(BuildContext context) async {
     try {
       final database = Provider.of<Database>(context, listen: false);
-      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+      await database.createJob(Job(
+        name: 'Blogging',
+        ratePerHour: 10,
+      ));
     } on PlatformException catch (e) {
       PlatformExceptionAlertDialog(
         title: 'Operation failed',
@@ -60,10 +63,30 @@ class JobsPage extends StatelessWidget {
           )
         ],
       ),
+      body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _createJob(context),
       ),
     );
   }
+}
+
+Widget _buildContents(BuildContext context) {
+  final database = Provider.of<Database>(context);
+  return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final jobs = snapshot.data;
+          final children = jobs.map((job) => Text(job.name)).toList();
+          return ListView(children: children);
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text(snapshot.error.toString()));
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      });
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:track_time/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements Database {
@@ -12,14 +13,26 @@ class FirestoreDatabase implements Database {
 
   final String uid;
 
-  Future<void> createJob(Job job) async => _setData(
+  Stream<List<Job>> jobsStream() {
+    final path = APIPath.jobs(uid);
+    final reference = Firestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.documents
+        .map(
+          (snapshot) => Job(
+              name: snapshot.data['name'],
+              ratePerHour: snapshot.data['ratePerHour']),
+        )
+        .toList());
+  }
+
+  Future<void> createJob(Job job) async => await _setData(
         path: APIPath.job(uid, 'job_123'),
         data: job.toMap(),
       );
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = Firestore.instance.document(path);
-    print('$path: $data');
     await reference.setData(data);
   }
 }
